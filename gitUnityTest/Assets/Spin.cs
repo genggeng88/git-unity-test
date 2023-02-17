d71ee256eb7e4ab75e799d1e0111ffba4f882f46
Qin Geng <gengqin50@gmail.com>
2023-02-17 13:34:32 -0800

    FPS building environment

---
+++ gitUnityTest/Assets/Spin.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+    public float speed = 3.0f;
+
+    void Update() {
+        transform.Rotate(0, speed, 0);
+    }
+}
